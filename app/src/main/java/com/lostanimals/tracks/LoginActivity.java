9d3384495ce1c95f6a0491784198f0a9ee7bd7d0
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-08-27 16:51:24 +1200

    fixed some getters and setter stuff in loginAct. fixed string xml tags

--- app/src/main/java/com/lostanimals/tracks/LoginActivity.java
+++ app/src/main/java/com/lostanimals/tracks/LoginActivity.java
@@ -7,53 +7,28 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText etUser, etPassword;
-    private String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Setup EditText fields to get properties from .xml
-
         setEtUser((EditText) findViewById(R.id.login_et_username));
         setEtPassword((EditText) findViewById(R.id.login_et_password));
-        // etUser = findViewById(R.id.etLoginUsername);
-        // etPassword = findViewById(R.id.etLoginPassword);
     }
 
     // This method is called when Login button is pressed
     public void OnLogin(View view) {
         // Get the text a user has entered.
-        setUsername(getEtUser().toString());
-        setPassword(getEtPassword().toString());
-
-
-        //String username = etUser.getText().toString();
-        //String password = etPassword.getText().toString();
+        String username = etUser.getText().toString();
+        String password = etPassword.getText().toString();
 
         // TODO: These could be enums depending on how many scripts will be called
         String type = "login";
 
         // Run the login script
         BackgroundWorker bgWorker = new BackgroundWorker(this);
-        bgWorker.execute(type, getUsername(), getPassword());
-    }
-
-    private String getPassword() {
-        return password;
-    }
-
-    private void setPassword(String password) {
-        this.password = password;
-    }
-
-    private String getUsername() {
-        return username;
-    }
-
-    private void setUsername(String username) {
-        this.username = username;
+        bgWorker.execute(type, username, password);
     }
 
     private EditText getEtUser() {
