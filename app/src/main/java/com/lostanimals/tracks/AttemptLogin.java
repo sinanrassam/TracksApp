58c224434ad37cfa9db9b3d46e3f9f61e1941f6c
Sinan <sinanrassam@outlook.com>
2019-09-04 13:27:25 +1200

    Finished creating a post, fixed a bug with AttemptLogin class

--- app/src/main/java/com/lostanimals/tracks/AttemptLogin.java
+++ app/src/main/java/com/lostanimals/tracks/AttemptLogin.java
@@ -71,8 +71,24 @@ public class AttemptLogin extends AsyncTask<String, Void, JSONObject> {
             Log.d("BackgroundWorker", "Entered new post state");
         } else if (type.equals("new post")) {
             Log.d("BackgroundWorker", "Entered new post state");
+            String postTitle = params[1];
+            String postDesc = params[2];
+            String username = params[3];
+
+            try {
+                // Create a connection to the server/register.php file
+                postData += URLEncoder.encode("title", "UTF-8") + "=" + URLEncoder.encode(postTitle, "UTF-8") + "&";
+                postData += URLEncoder.encode("description", "UTF-8") + "=" + URLEncoder.encode(postDesc, "UTF-8") + "&";
+                postData += URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
+
+                json = processRequest(SCRIPT_URL + "post.php", postData);
+            } catch (IOException | JSONException e) {
+                // TODO: Maybe Log these?
+                e.printStackTrace();
+            }
         } else if (type.equals("get post")) {
-            Log.d("BackgroundWorker", "Entered new post state");
+            Log.d("BackgroundWorker", "Entered get post state");
+
         }
         return json;
     }
@@ -92,31 +108,39 @@ public class AttemptLogin extends AsyncTask<String, Void, JSONObject> {
      */
     @Override
     protected void onPostExecute(JSONObject data) {
-        try {
-            if (data.get("response").equals("successful")) {
-                Log.d("onPostExecute", "success");
-                if (data.get("purpose").equals("login")) {
-                    alertDialog.setTitle("Login Status");
-                    JSONObject details = (JSONObject) data.get("details");
-                    Log.d("details: ", (String) details.get("name"));
-                    Log.d("details: ", (String) details.get("username"));
-                    Log.d("details: ", (String) details.get("email"));
-                    alertDialog.setMessage("Login successful");
-                    alertDialog.show();
-                } else if (data.get("purpose").equals("login")) {
-                    alertDialog.setTitle("Login Status");
-                    JSONObject details = (JSONObject) data.get("details");
-                    Log.d("details: ", (String) details.get("name"));
-                    Log.d("details: ", (String) details.get("username"));
-                    Log.d("details: ", (String) details.get("email"));
-                    alertDialog.setMessage("Login successful");
+        if (data != null) {
+            try {
+                if (data.get("response").equals("successful")) {
+                    Log.d("onPostExecute", "success");
+                    if (data.get("purpose").equals("login")) {
+                        alertDialog.setTitle("Login Status");
+                        JSONObject details = (JSONObject) data.get("details");
+                        Log.d("details: ", (String) details.get("name"));
+                        Log.d("details: ", (String) details.get("username"));
+                        Log.d("details: ", (String) details.get("email"));
+                        alertDialog.setMessage("Login successful");
+                        alertDialog.show();
+                    } else if (data.get("purpose").equals("register")) {
+                        alertDialog.setTitle("Register Status");
+                        JSONObject details = (JSONObject) data.get("details");
+                        Log.d("details: ", (String) details.get("name"));
+                        Log.d("details: ", (String) details.get("username"));
+                        Log.d("details: ", (String) details.get("email"));
+                        alertDialog.setMessage("Register successful");
+                        alertDialog.show();
+                    } else if (data.get("purpose").equals("new post")) {
+                        alertDialog.setTitle("New Post Status");
+                        alertDialog.setMessage("New post successful");
+                        alertDialog.show();
+                    }
+                } else {
+                    alertDialog.setTitle("Error");
+                    alertDialog.setMessage("Operation Failed");
                     alertDialog.show();
                 }
-            } else {
-                Log.d("onPostExecute", "fail");
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
     }
 
@@ -142,7 +166,6 @@ public class AttemptLogin extends AsyncTask<String, Void, JSONObject> {
         // A return will be sent, so create an input stream to capture this and read it.
         InputStream inputStream = conn.getInputStream();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
-        String result = "";
         String line;
 
         StringBuilder sb = new StringBuilder();
--- app/src/main/java/com/lostanimals/tracks/NewPostActivity.java
+++ app/src/main/java/com/lostanimals/tracks/NewPostActivity.java
@@ -2,12 +2,29 @@ package com.lostanimals.tracks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class NewPostActivity extends AppCompatActivity {
+    private EditText etTitle, etDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        etTitle = (EditText) findViewById(R.id.post_et_post_title);
+        etDescription = (EditText) findViewById(R.id.post_et_desc);
+    }
+
+    public void onNewPost(View view) {
+        String title = etTitle.getText().toString();
+        String description = etDescription.getText().toString();
+        String username = "USER";
+
+        // Run the register script
+        AttemptLogin bgWorker = new AttemptLogin(this);
+        bgWorker.execute("new post", title, description, username);
     }
 }
