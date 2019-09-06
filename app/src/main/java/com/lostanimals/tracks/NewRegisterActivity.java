6955ecdd8a0c2add27a75f9740aa219ec34a6424
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-09-06 16:03:36 +1200

    added email validator

--- app/src/main/java/com/lostanimals/tracks/NewRegisterActivity.java
+++ app/src/main/java/com/lostanimals/tracks/NewRegisterActivity.java
@@ -2,37 +2,20 @@ package com.lostanimals.tracks;
 
 import android.annotation.TargetApi;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import com.lostanimals.tracks.utils.EmailValidator;
 import com.lostanimals.tracks.utils.PreferencesUtility;
 import com.lostanimals.tracks.utils.ServerManager;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
-/**
- * A login screen that offers login via email/password.
- */
 public class NewRegisterActivity extends AppCompatActivity {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
 
     private ServerManager mServerTask = null;
     private PreferencesUtility mPreferencesUtility;
@@ -52,13 +35,13 @@ public class NewRegisterActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mPreferencesUtility = new PreferencesUtility(sharedPreferences);
 
-        mFirstNameView = (EditText) findViewById(R.id.firstName);
-        mLastNameView = (EditText) findViewById(R.id.lastName);
-        mEmailView = (EditText) findViewById(R.id.email);
-        mUsernameView = (EditText) findViewById(R.id.username);
-        mPasswordView = (EditText) findViewById(R.id.password);
+        mFirstNameView = findViewById(R.id.firstName);
+        mLastNameView = findViewById(R.id.lastName);
+        mEmailView = findViewById(R.id.email);
+        mUsernameView = findViewById(R.id.username);
+        mPasswordView = findViewById(R.id.password);
 
-        Button mRegisterBtn = (Button) findViewById(R.id.register_btn);
+        Button mRegisterBtn = findViewById(R.id.register_btn);
         mRegisterBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,9 +50,7 @@ public class NewRegisterActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
+
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void setupActionBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -78,11 +59,6 @@ public class NewRegisterActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptRegister() {
 
         // Reset errors.
@@ -115,6 +91,10 @@ public class NewRegisterActivity extends AppCompatActivity {
         }
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!(EmailValidator.isValidEmail(email))) {
+            mEmailView.setError(getString(R.string.email_incorrect));
             focusView = mEmailView;
             cancel = true;
         }
