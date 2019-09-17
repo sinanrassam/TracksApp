2f325667a198ad55c8aba1287adf6c1cb17ca060
calcalbrad <callumbradding@gmail.com>
2019-09-17 16:09:26 +1200

    Adding MyPostActivity (MyPosts/Profile) Java File, activity_my_posts and nav_menu XML Files & added data to strings XML file.

---
+++ app/src/main/java/com/lostanimals/tracks/MyPostActivity.java
@@ -0,0 +1,60 @@
+package com.lostanimals.tracks;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MyPostActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_posts);
+
+        // Setup DrawerLayout and ActionBar
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_action_bar,
+                R.string.close_action_bar);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TODO: Create navigation view XML
+        //NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.home_nav) {
+
+        }
+
+        if (id == R.id.setting_nav) {
+
+        }
+
+        if (id == R.id.logOut_nav) {
+
+        }
+
+        return false;
+    }
+}
