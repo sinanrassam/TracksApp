e90173549851f635e068f0dd13d723d8155e60ff
Sinan Rassam <sinanrassam@outlook.com>
2019-09-17 15:55:57 +1200

    Working on PostEntryActivity, currently causing the app to crash for no apparent reason

--- app/src/main/java/com/lostanimals/tracks/PostEntryActivity.java
+++ app/src/main/java/com/lostanimals/tracks/PostEntryActivity.java
@@ -2,12 +2,40 @@ package com.lostanimals.tracks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.lostanimals.tracks.utils.PostEntry;
 
 public class PostEntryActivity extends AppCompatActivity {
+
+    private PostEntry mPostEntry;
+    private TextView mPostTitleView, mPostDescView, mPostDateView, mPostAuthorView;
+
+    public PostEntryActivity() {
+        super();
+    }
+
+    public PostEntryActivity(PostEntry postEntry) {
+        this();
+        mPostEntry = postEntry;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
+        setContentView(R.layout.activity_post_entry);
+
+        mPostTitleView = findViewById(R.id.post_txt_title);
+        mPostDescView = findViewById(R.id.post_et_desc);
+        mPostDateView = findViewById(R.id.post_date);
+        mPostAuthorView = findViewById(R.id.post_author);
+
+//        mPostEntry = new PostEntry("title", "title");
+
+        mPostTitleView.setText(mPostEntry.getPostTitle());
+
+        mPostDescView.setText("mPostEntry.getPostDesc()");
+        mPostDateView.setText("Posted on: " + "mPostEntry.getPostDesc()");
+        mPostAuthorView.setText("By: " + "mPostEntry.getUsername()");
     }
 }
