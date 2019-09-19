439560145a5733cd0e2bfed34e7944aeb37291b2
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-09-19 13:09:33 +1200

    starting on rebuilding unit tests

--- app/src/test/java/com/lostanimals/tracks/ConnectionTest.java
+++ app/src/test/java/com/lostanimals/tracks/ConnectionTest.java
@@ -1,34 +1,33 @@
-//package com.lostanimals.tracks;
-//
-//import android.content.Context;
-//import com.lostanimals.tracks.utils.ConnectionManager;
-//import com.lostanimals.tracks.utils.ServerManager_Backup;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//
-//import java.io.IOException;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class ConnectionTest {
-//    private static final String TEST_URL = "http://bosh.live:7536/phpmyadmin/tracks_api/";
-//
-//    @Mock
+package com.lostanimals.tracks;
+
+        import android.content.Context;
+        import com.lostanimals.tracks.utils.ConnectionManager;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.mockito.Mock;
+        import org.mockito.runners.MockitoJUnitRunner;
+
+        import java.io.IOException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConnectionTest {
+    private static final String TEST_URL = "http://bosh.live:7536/phpmyadmin/tracks_api/";
+
+    @Mock
 //    ServerManager_Backup mockServerManager;
-//    ConnectionManager mockConnectionManager;
-//    @Mock
-//    Context mockContext;
-//
-//    @Before
-//    public void initMocks() {
+    ConnectionManager mockConnectionManager;
+    @Mock
+    Context mockContext;
+
+    @Before
+    public void initMocks() {
 //        mockServerManager = new ServerManager_Backup(mockContext);
-//    }
-//
-//    // TODO: Fix this
-//    @Test
-//    public void ServerManager_OpenConnectionTest() throws IOException {
-//
-//    }
-//}
+    }
+
+    // TODO: Fix this
+    @Test
+    public void ServerManager_OpenConnectionTest() throws IOException {
+
+    }
+}
