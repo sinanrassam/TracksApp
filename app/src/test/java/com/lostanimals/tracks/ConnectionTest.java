4af9cc5ff89d9a7f48dfec6ba3a22f84b4c8096f
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-09-19 16:46:37 +1200

    Added Nav drawer with Nitesh

--- app/src/test/java/com/lostanimals/tracks/ConnectionTest.java
+++ app/src/test/java/com/lostanimals/tracks/ConnectionTest.java
@@ -1,32 +1,33 @@
 package com.lostanimals.tracks;
 
-        import android.content.Context;
-        import com.lostanimals.tracks.utils.ConnectionManager;
-        import com.lostanimals.tracks.utils.tests.ConnMan;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-        import org.mockito.Mock;
-        import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-        import java.io.IOException;
+import java.io.IOException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConnectionTest {
-    private static final String TEST_URL = "http://bosh.live:7536/phpmyadmin/tracks_api/";
+    private static final String URL = "http://bosh.live:7536/phpmyadmin/tracks_api/connTest.php";
 
     @Mock
-    ConnMan mockConnMan;
+    ConnectionManager mockConnMan;
 
     @Before
     public void initMocks() {
-        mockConnMan = new ConnMan();
+        mockConnMan = new ConnectionManager();
     }
 
     // TODO: Fix this
     @Test
     public void ConnectionManager_OpenConnection() throws IOException {
-
+        // assertTrue(mockConnMan.openConnection(URL+"Hi").getResponseMessage());
+        mockConnMan.openConnection(URL+"blaj");
     }
 
     @Test
