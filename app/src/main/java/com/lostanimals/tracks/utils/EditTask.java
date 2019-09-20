8a7bcac1711ba9b70611c788453c08e164cf0161
calcalbrad <callumbradding@gmail.com>
2019-09-20 17:17:32 +1200

    Added Response to JSONObject in NewPostTask

--- app/src/main/java/com/lostanimals/tracks/utils/EditTask.java
+++ app/src/main/java/com/lostanimals/tracks/utils/EditTask.java
@@ -34,10 +34,7 @@ public class EditTask extends AsyncTask<String, Integer, JSONObject> {
 
     @Override
     protected void onPostExecute(JSONObject data) {
-        //TODO: Something with the returned JSONObject. Maybe check result is successful, if so make a toast / notify
-        // the calling UI with an adapter. Maybe go back to the post and refresh it so the update is visible.
         try {
-            //TODO: See if we need to return the post
             if (data.get("response").equals("successful")) {
                 Toast.makeText(mContext, "Post Successfully Updated", Toast.LENGTH_SHORT).show();
             }
