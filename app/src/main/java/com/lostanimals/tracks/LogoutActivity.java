fe3e9a2b5e950046e605321e8f76c3c13258912e
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-09-25 17:50:46 +1200

    Migrated to AndroidX. Tested all activities.

--- app/src/main/java/com/lostanimals/tracks/LogoutActivity.java
+++ app/src/main/java/com/lostanimals/tracks/LogoutActivity.java
@@ -1,9 +1,7 @@
 package com.lostanimals.tracks;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,15 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.lostanimals.tracks.utils.PreferencesUtility;
 
 public class LogoutActivity extends AppCompatActivity {
-	private PreferencesUtility mPreferencesUtility;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_logout);
-		
-		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-		mPreferencesUtility = new PreferencesUtility(sharedPreferences);
 		
 		attemptLogout();
 		
