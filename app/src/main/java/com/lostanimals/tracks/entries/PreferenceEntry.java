7a0628be9aecf8c8d398c479c8be706417107645
nitesh <niteshkc132@gmail.com>
2019-10-03 13:26:51 +1300

    history tab added

--- app/src/main/java/com/lostanimals/tracks/entries/PreferenceEntry.java
+++ app/src/main/java/com/lostanimals/tracks/entries/PreferenceEntry.java
@@ -1,10 +1,13 @@
 package com.lostanimals.tracks.entries;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PreferenceEntry {
 	private static String mName = null;
 	private static String mUsername = null;
 	private static String mEmail = null;
-	
+	//private static Queue<Integer> mHistory = new LinkedList<>();
 	public PreferenceEntry(String name, String username, String email) {
 		mName = name;
 		mUsername = username;
