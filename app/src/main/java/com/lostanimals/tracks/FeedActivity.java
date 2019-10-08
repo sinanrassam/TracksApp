c99d73792d7361fad433e878c33d8dea31289d04
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-10-08 13:39:10 +1300

    Added case to the menu for opening basic map activity.

--- app/src/main/java/com/lostanimals/tracks/FeedActivity.java
+++ app/src/main/java/com/lostanimals/tracks/FeedActivity.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatTextView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
-import com.lostanimals.tracks.utils.PreferencesUtility;
 
 import java.util.Objects;
 
@@ -37,13 +34,6 @@ public class FeedActivity extends AppCompatActivity implements NavigationView.On
 		
 		NavigationView navigationView = findViewById(R.id.navigation_view);
 		navigationView.setNavigationItemSelectedListener(this);
-
-//		View header = navigationView.getHeaderView(0);
-//		AppCompatTextView mUsername = header.findViewById(R.id.usernameHeader);
-//		AppCompatTextView mEmail = header.findViewById(R.id.emailHeader);
-		
-//		mUsername.setText(PreferencesUtility.getUserInfo().getUsername());
-//		mEmail.setText(PreferencesUtility.getUserInfo().getEmail());
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setTitle("Feed");
@@ -81,6 +71,8 @@ public class FeedActivity extends AppCompatActivity implements NavigationView.On
 			case R.id.myProfile_nav:
 				navigationIntent = new Intent(this, MyProfileActivity.class);
 				break;
+			case R.id.map_nav:
+				navigationIntent = new Intent(this, MapsActivity.class);
 		}
 		
 		if (navigationIntent != null) {
