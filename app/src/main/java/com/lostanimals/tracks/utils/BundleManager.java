6b4257123eca461d4f845dddb068536dc52657bd
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-10-08 16:21:22 +1300

    added skeleton methods in BundleMan for setting and getting details. To obfuscate the bundle itself.

--- app/src/main/java/com/lostanimals/tracks/utils/BundleManager.java
+++ app/src/main/java/com/lostanimals/tracks/utils/BundleManager.java
@@ -4,4 +4,12 @@ import android.os.Bundle;
 
 public class BundleManager {
     public static Bundle mPostBundle = new Bundle();
+
+    public void setPostID(String newPostID) {
+
+    }
+
+    public void setPostLocation() {
+
+    }
 }
