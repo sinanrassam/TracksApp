d338991f7da7dd733b07f75db9348bd8ff0c5be7
Sinan Rassam <sinanrassam@outlook.com>
2019-10-09 13:35:17 +1300

    Completed a rough idea of how the GetFollowedPostsTask work

--- app/src/main/java/com/lostanimals/tracks/tasks/GetFollowedPostsTask.java
+++ app/src/main/java/com/lostanimals/tracks/tasks/GetFollowedPostsTask.java
@@ -1,4 +1,66 @@
 package com.lostanimals.tracks.tasks;
 
-public class GetFollowedPostsTask {
+import android.os.AsyncTask;
+import android.util.Log;
+import com.lostanimals.tracks.entries.PostEntry;
+import com.lostanimals.tracks.utils.ConnectionManager;
+import com.lostanimals.tracks.utils.PostsUtility;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.lostanimals.tracks.utils.ConnectionManager.processRequest;
+
+public class GetFollowedPostsTask extends AsyncTask<String, Integer, Boolean> {
+    private List<String> mFollowedPostsList;
+    @Override
+    protected Boolean doInBackground(String... parameters) {
+        boolean success = true;
+        JSONObject json = null;
+        if (!this.isCancelled()) {
+            String postData = null;
+            try {
+                postData = ConnectionManager.postEncoder("get-followed-posts", parameters);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                success = false;
+            }
+
+            try {
+                json = processRequest("follow.php", postData);
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+                success = false;
+            }
+        }
+
+        if (json != null) {
+            PostsUtility.clear();
+            mFollowedPostsList = new ArrayList<>();
+            try {
+                JSONArray jsonArray = (JSONArray) json.get("posts");
+                Log.d("test", jsonArray.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                    mFollowedPostsList.add(jsonObject.toString());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                success = false;
+            }
+        }
+        return success;
+    }
+
+    public List<String> getFollowedPosts() {
+        return mFollowedPostsList;
+    }
 }
