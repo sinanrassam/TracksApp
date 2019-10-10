498a0f89b9e2339b32ca40412bc4db7784242d0d
calcalbrad <callumbradding@gmail.com>
2019-10-10 15:39:26 +1300

    Custom dialog finished in FiltersFragment

--- app/src/main/java/com/lostanimals/tracks/FiltersFragment.java
+++ app/src/main/java/com/lostanimals/tracks/FiltersFragment.java
@@ -2,7 +2,9 @@ package com.lostanimals.tracks;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
@@ -10,7 +12,20 @@ public class FiltersFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.filters_fragment, null))
+                .setPositiveButton(R.string.filters_confirm, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Filter the posts based on the selection
+                    }
+                })
+                .setNegativeButton(R.string.filters_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FiltersFragment.this.getDialog().cancel();
+                    }
+                });
         return builder.create();
     }
 }
