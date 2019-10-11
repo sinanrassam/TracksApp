0fa7827cb1b0c3477c4da643a12e77515c27648c
JasonChun <53282149+JasonChun@users.noreply.github.com>
2019-10-11 16:38:20 +1300

    Commenting

--- app/src/main/java/com/lostanimals/tracks/LoginActivity.java
+++ app/src/main/java/com/lostanimals/tracks/LoginActivity.java
@@ -24,6 +24,15 @@ import java.util.concurrent.ExecutionException;
 /**
  *
  */
+// Login activity class to execute logging in
+
+// Setting feedIntent and registerIntent to switch activities depending on the
+// users actions, clicking on login or register button
+
+// onClick listener to check if user clicked Register or Login
+// If login button, then attempt to login and check for valid login information
+// If register button, then start register activity using registerIntent
+
 public class LoginActivity extends AppCompatActivity {
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -35,6 +44,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Creation of action bar with Login as title
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Login");
 
@@ -43,6 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         registerIntent = new Intent(this, RegisterActivity.class);
         registerIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 
+        // Creation of pendingIntent to enable notifications
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, feedIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationUtility.createNotification(this, "Tracks", "Test", true, pendingIntent);
         PreferencesUtility.setSharedPreferences(this);
@@ -60,6 +71,9 @@ public class LoginActivity extends AppCompatActivity {
         mEmailView = findViewById(R.id.login_username);
         mPasswordView = findViewById(R.id.login_password);
 
+        // Listening for click on login button
+        // If clicked, call attemptLogin() to attempt login
+        // If exception is returned, catch and print what exception was thrown
         Button mSignInBtn = findViewById(R.id.login_btn);
         mSignInBtn.setOnClickListener(new OnClickListener() {
             /**
@@ -77,6 +91,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Listening for click on register button
+        // If this was clicked, start register activity using registerIntent
         Button mRegisterBtn = findViewById(R.id.login_register_btn);
         mRegisterBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -92,6 +108,11 @@ public class LoginActivity extends AppCompatActivity {
      * @throws InterruptedException
      * @throws JSONException
      */
+
+    // attemptLogin()
+    //~~~~~~~~~~~~~~~~
+    // Attempt to login using the information that the user has entered (email and password)
+    // Throw exceptions for onCreate methods to catch to print exceptions to re-evaluate
     private void attemptLogin() throws ExecutionException, InterruptedException, JSONException {
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -111,6 +132,7 @@ public class LoginActivity extends AppCompatActivity {
             cancel = true;
         }
 
+        // Check if valid email has been entered, if not display error
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
@@ -124,6 +146,7 @@ public class LoginActivity extends AppCompatActivity {
             cancel = true;
         }
 
+        // Checking if loginTask was executed successfully, if it was then display notification and start feed activity
         if (cancel) {
             focusView.requestFocus();
         } else {
