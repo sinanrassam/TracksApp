0a033c4dd5a567fb724e803f12a100d097fdcbbe
JasonChun <53282149+JasonChun@users.noreply.github.com>
2019-10-14 11:45:50 +1300

    onSharedPreferenceChanged for RyeRye

--- app/src/main/java/com/lostanimals/tracks/SettingsFragment.java
+++ app/src/main/java/com/lostanimals/tracks/SettingsFragment.java
@@ -1,19 +1,32 @@
 package com.lostanimals.tracks;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.Nullable;
+import androidx.preference.PreferenceScreen;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 
-public class SettingsFragment  extends PreferenceFragmentCompat{
+public class SettingsFragment  extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private String logout = "logout_KEY";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference, rootKey);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        PreferenceScreen ps = (PreferenceScreen) findPreference(logout)
+
+        if(key.equals(logout)){
+
+        }
     }
 }
