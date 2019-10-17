17e4d4cbd3b49033b711848cb46381c7350a2060
Ryan Bosher <ryan.julian.bosher@icloud.com>
2019-10-18 00:13:24 +1300

    Tidy branch of unused resources to minimize conflicts on pull req.

--- app/src/test/java/com/lostanimals/tracks/PreferenceUtilityTest.java
+++ app/src/test/java/com/lostanimals/tracks/PreferenceUtilityTest.java
@@ -12,23 +12,23 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-@RunWith (MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PreferenceUtilityTest {
-	
-	@Mock
-	Context mockContext;
-	// The class to be tested.
-	private PreferencesUtility preferencesUtility;
-	private Context context = mock(Context.class);
-	
-	@Before
-	public void setup() {
-		//PreferencesUtility.setSharedPreferences(context);
-	}
-	
-	@Test
-	public void PreferenceUtility_FindExistingCustomer() {
-		PreferencesUtility.setSharedPreferences(context);
-		assertTrue(PreferencesUtility.setUserInfo(new PreferenceEntry("admin", "admin", "admin")));
-	}
+
+    @Mock
+    Context mockContext;
+    // The class to be tested.
+    private PreferencesUtility preferencesUtility;
+    private Context context = mock(Context.class);
+
+    @Before
+    public void setup() {
+        //PreferencesUtility.setSharedPreferences(context);
+    }
+
+    @Test
+    public void PreferenceUtility_FindExistingCustomer() {
+        PreferencesUtility.setSharedPreferences(context);
+        assertTrue(PreferencesUtility.setUserInfo(new PreferenceEntry("admin", "admin", "admin")));
+    }
 }
