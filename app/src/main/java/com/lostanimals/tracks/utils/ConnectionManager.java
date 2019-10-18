aacbdbaf2490c9b89f0fd3647decac120c859a4b
calcalbrad <callumbradding@gmail.com>
2019-10-19 12:17:14 +1300

    Updated ConnMan to send filters command to PHP script

--- app/src/main/java/com/lostanimals/tracks/utils/ConnectionManager.java
+++ app/src/main/java/com/lostanimals/tracks/utils/ConnectionManager.java
@@ -26,7 +26,7 @@ public class ConnectionManager {
 				postData += URLEncoder.encode("number", "UTF-8") + "=" + URLEncoder.encode(parameters[0], "UTF-8") + "&";
 				postData += URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(parameters[1], "UTF-8") + "&";
 				postData += URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(parameters[2], "UTF-8") + "&";
-				postData += URLEncoder.encode("myPosts", "UTF-8") + "=" + URLEncoder.encode(parameters[3], "UTF-8");
+				postData += URLEncoder.encode("myPosts", "UTF-8") + "=" + URLEncoder.encode(parameters[3], "UTF-8") + "&";
 				postData += URLEncoder.encode("filters", "UTF-8") + "=" + URLEncoder.encode(parameters[4], "UTF-8");
 				break;
 			case "new-comment":
